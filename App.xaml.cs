54fd0d0bd85305ef183784c60b765680abcea246
Tom Eden <tom.delikat.eden@gmail.com>
2025-04-24 18:20:26 +0300

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TurboTyper;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
