7bd4c6752d0752827e316ea36d70887d0e648415
Tom Eden <tom.delikat.eden@gmail.com>
2025-04-25 12:10:17 +0300

    Custom Window for Main Page

---
+++ MVVM/View/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TurboTyper;
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TitelBar_Drag(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+            DragMove();
+    }
+}
