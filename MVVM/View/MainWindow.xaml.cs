abfa9399512a78785fedd17e47633dcd702d6c5c
Tom Eden <tom.delikat.eden@gmail.com>
2025-04-26 21:27:51 +0300

    None functional Launch page

--- MVVM/View/MainWindow.xaml.cs
+++ MVVM/View/MainWindow.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using TurboTyper.MVVM.View.Pages;
 
-namespace TurboTyper;
+namespace TurboTyper.MVVM.View;
 
 
 public partial class MainWindow : Window
@@ -9,7 +10,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        MainFrame.Navigate(new LaunchPage());
     }
+
 
     private void TitelBar_Drag(object sender, MouseButtonEventArgs e)
     {
---
+++ MVVM/View/Pages/LaunchPage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TurboTyper.MVVM.View.Pages;
+
+public partial class LaunchPage : Page
+{
+    public LaunchPage()
+    {
+        InitializeComponent();
+    }
+}
